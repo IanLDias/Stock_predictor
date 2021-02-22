55753acffbecbdf9506698c846e962c16e08c71d
IanLDias <idias176@gmail.com>
2021-02-22 17:53:27 +1000

    Added cached functions

--- config_var.py
+++ config_var.py
@@ -11,17 +11,3 @@ today = date.today().strftime("%Y-%m-%d")
 #az acr build --registry StockPredictorRegistry --resource-group StockPredictor --image main .
 
 #az webapp config appsettings set --resource-group StockPredictor --name stock-predictor-web-app --settings WEBSITES_PORT=80
-
-
-
-# WEBSITES_CONTAINER_START_TIME_LIMIT=1800
-# disableWatchdogWarning = false
-# sharingMode = “off”
-# showWarningOnDirectExecution = true
-# logLevel = “debug”
-# caching = true
-# displayEnabled = true
-# magicEnabled = true
-# fixMatplotlib = true
-# port = 80
-# serverPort = 80
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import streamlit as st
 
-    
 from Data.raw_data import load_data, plot_raw_data
 from Data.forecast import forecast
 from Data.news import news
-from config_var import today
 from Data.symbol_names import stocks, get_information
+
+from config_var import today
+
 from fbprophet.plot import plot_plotly
+
 
 #Drop down menu of stocks (ticker form)
 stock_name = st.sidebar.selectbox("Select dataset for prediction", list(stocks.keys()))
@@ -15,44 +17,64 @@ stock_ticker = stocks[stock_name]
 
 st.title(f"Stock Prediction for {stock_name}")
 
+st.header("Description")
 st.write(get_information(stock_name))
-#analysis = ["Raw Data", "Forecast Data", "News"]
-#selected_analysis = st.selectbox("Analysis", analysis)
+
 
 #Loads the data associated with the stock
-data = load_data(stock_ticker)
+@st.cache
+def data_load(stock_ticker):
+    print("CACHE MISS DATA_LOAD")
+    return load_data(stock_ticker)
 
+data = data_load(stock_ticker)
+
+@st.cache
+def forecast_func(n_years, data):
+    print('FIRST CACHE OF FORECAST')
+    return forecast(n_years, data)
+
+@st.cache(allow_output_mutation=True)
+def forecast_graph(m, forecast):
+
+    return plot_plotly(m, forecast)
+
+@st.cache(allow_output_mutation=True)
+def forecast_components_graph(forecast_local):
+    print("**COMPONENTS FIRST CACHE**")
+    return m.plot_components(forecast_local)
+
+@st.cache
+def news_func():   
+    articles = news(stock_name)
+    return articles
+    
 #Plotting the raw data
-def raw_data():
-    st.subheader('Raw data')
-    st.write(data.tail())
 
+#How far into the future to predict
+
+if st.sidebar.checkbox('Raw data'):
+    st.subheader('Raw data')
+    st.dataframe(data.tail())
     fig = plot_raw_data(data)
     st.plotly_chart(fig)
 
-#How far into the future to predict
-raw_data()
-n_years = st.slider("Years of prediction", 1, 4)
+if st.sidebar.checkbox('Forecast'):
+    n_years = st.slider("Years of prediction", 1, 4)
+    m, forecast = forecast_func(n_years, data)
 
-#Uses Prophet to predict
-m, forecast = forecast(n_years, data)
+    st.subheader('Forecast data')
+    #Changed to local to be able to implement caching for forecast_graph
+    st.write(forecast.tail())
+    st.write('Forecast data')
+    st.plotly_chart(forecast_graph(m, forecast))
 
-#Plots the forecast
-st.subheader('Forecast data')
-st.write(forecast.tail())
+    st.write('Forecast components')
+    st.write(forecast_components_graph(forecast))
 
-st.write('Forecast data')
-forecast_fig = plot_plotly(m, forecast)
-st.plotly_chart(forecast_fig)
-
-st.write('Forecast components')
-components_fig = m.plot_components(forecast)
-st.write(components_fig)
-
-st.title(f"News for {stock_name}")   
-articles = news(stock_name)
-for article in articles[:10]:
-    st.subheader(article['title'])
-    st.markdown(article['description'])
-
-#Table of the relevant news associated with the stock
+if st.sidebar.checkbox('News'):
+    articles = news_func()
+    st.title(f"News for {stock_name}")
+    for article in articles[:10]:
+        st.subheader(article['title'])
+        st.markdown(article['description'])
